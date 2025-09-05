b1381c2a731cdfe6ec9614041b392068fd0df277
Zehao Lu <luzehao1998@gmail.com>
2025-09-05 23:39:31 +0000

    cleanup helloworld

--- llmcpp/helloworld.cu
+++ llmcpp/helloworld.cu
@@ -29,58 +29,6 @@ __global__ void hello_kernel()
     printf("Hello World from GPU thread %d!\n", threadIdx.x);
 }
 
-// // CUPTI buffer request callback
-// void CUPTIAPI bufferRequested(uint8_t **buffer, size_t *size, size_t *maxNumRecords)
-// {
-//     *size = 16 * 1024;
-//     *buffer = (uint8_t *)malloc(*size);
-//     *maxNumRecords = 0; // unlimited
-// }
-
-// // CUPTI buffer complete callback
-// void CUPTIAPI bufferCompleted(CUcontext ctx, uint32_t streamId,
-//                               uint8_t *buffer, size_t size, size_t validSize)
-// {
-//     CUptiResult status;
-//     CUpti_Activity *record = NULL;
-
-//     // Iterate over records in the buffer
-//     while (1)
-//     {
-//         status = cuptiActivityGetNextRecord(buffer, validSize, &record);
-//         if (status == CUPTI_SUCCESS)
-//         {
-//             if (record->kind == CUPTI_ACTIVITY_KIND_CONCURRENT_KERNEL)
-//             {
-//                 CUpti_ActivityKernel8 *kernel = (CUpti_ActivityKernel8 *)record;
-//                 printf("CUPTI: Kernel \"%s\" launched on stream %u, grid (%u,%u,%u), block (%u,%u,%u)\n",
-//                        kernel->name, kernel->streamId,
-//                        kernel->gridX, kernel->gridY, kernel->gridZ,
-//                        kernel->blockX, kernel->blockY, kernel->blockZ);
-//             }
-//         }
-//         else if (status == CUPTI_ERROR_MAX_LIMIT_REACHED)
-//         {
-//             printf("CUPTI: Maximum buffer limit reached\n");
-//             break;
-//         }
-//         else
-//         {
-//             CUPTI_CALL(status);
-//         }
-//     }
-
-//     // Report dropped records
-//     size_t dropped;
-//     cuptiActivityGetNumDroppedRecords(ctx, streamId, &dropped);
-//     if (dropped != 0)
-//     {
-//         printf("CUPTI: Dropped %zu activity records\n", dropped);
-//     }
-
-//     free(buffer);
-// }
-
 __global__ void vecAdd(const float *A, const float *B, float *C, int numElements)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
@@ -131,6 +79,7 @@ __global__ void saxpy_more_compute(int n, float a, float *x, float *y)
          i < n; 
          i += blockDim.x * gridDim.x) 
       {
+        // 20 multiply-add operations
           y[i] = a * x[i] + y[i];
           y[i] = a * x[i] + y[i];
           y[i] = a * x[i] + y[i];
@@ -183,6 +132,9 @@ void launch_add()
 {
     size_t size = N * sizeof(float);
 
+    // No need to initialize the input data, as we are not copying from host to device
+    // If N is too large, these host arrays lead to segment fault
+
     // Host vectors
     // float h_A[N], h_B[N], h_C[N];
     // for (int i = 0; i < N; i++)
@@ -224,11 +176,10 @@ void launch_add()
     // cudaMemcpy(d_A_4, h_A, size, cudaMemcpyHostToDevice);
     // cudaMemcpy(d_B_4, h_B, size, cudaMemcpyHostToDevice);
     // cudaMemcpy(d_C_4, h_C, size, cudaMemcpyHostToDevice);
-    // Launch kernel
-    int threadsPerBlock = 512;
-    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
     cudaDeviceSynchronize();
+
+    // 3 sets of tests for GMP
 
     // Normal
     // GmpProfiler::getInstance()->pushRange("saxpy1", GmpProfileType::CONCURRENT_KERNEL);
@@ -284,26 +235,17 @@ int main(int argc, char** argv)
     for(int i=2; i<argc; i++){
         GmpProfiler::getInstance()->addMetrics(argv[i]);
     }
-    
+
     hello_kernel<<<1, 4>>>();
     int curr_pass = 0;
     GmpProfiler::getInstance()->init();
 
     printf("Starting profiling runs...\n");
-#ifdef USE_CUPTI
-// while (GmpProfiler::getInstance()->isAllPassSubmitted() == false)
-// {
-#endif
     printf("current pass: %zu\n", curr_pass++);
     GmpProfiler::getInstance()->startRangeProfiling();
-    // for (int i = 0; i < 1000; i++)
-    // {
-        launch_add();
-    // }
+    launch_add();
     GmpProfiler::getInstance()->stopRangeProfiling();
-#ifdef USE_CUPTI
-// }
-#endif
+
     cudaDeviceSynchronize();
     GmpProfiler::getInstance()->decodeCounterData();
     GmpProfiler::getInstance()->printProfilerRanges();
